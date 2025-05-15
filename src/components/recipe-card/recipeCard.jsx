0dce2f8fc72070f1ab152933cfcff0b6b12982e8
AirinNila <airinakhternila>
2025-05-15 18:09:19 +0600

    added card

--- src/components/recipe-card/recipeCard.jsx
+++ src/components/recipe-card/recipeCard.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { IoTimeOutline } from "react-icons/io5";
+import { AiOutlineFire } from "react-icons/ai";
 
 const RecipeCard = ({card}) => {
-    // console.log(card)
+     console.log(card.ingredients)
     const {recipe_id, recipe_name, image, description, ingredients, preparing_time, calories} = card;
 
     return (
@@ -9,7 +11,38 @@ const RecipeCard = ({card}) => {
             <div>
                 <img src={image} alt="recipe" className='rounded-lg' />
             </div>
-            <div></div>
+            <div className='flex flex-col gap-4'>
+                <div className='flex flex-col gap-4'>
+                    <h3 className='tittle text-xl font-semibold'>{recipe_name}</h3>
+                    <p className='des text-base'>{description}</p>
+                </div>
+                <div className='pt-6 flex flex-col gap-4 border-t-2 pb-4'>
+                    <h3 className='text-xl font-semibold'>Ingredients {ingredients.lenght}</h3>
+                    <ul className='list-disc text-[#878787] pl-6'>
+                        
+                        <li>{ingredients[0]}</li>
+                        <li>{ingredients[1]}</li>
+                        <li>{ingredients[2]}</li>
+                        <li>{ingredients[3]}</li>
+                        <li>{ingredients[4]}</li>
+                    </ul>
+                </div>
+                <div className='border-t-2 pt-6'>
+                    <div className='flex gap-4 text-[#282828CC]'>
+                        <div className='flex items-center gap-2'>
+                          <div className='text-xl'> <IoTimeOutline /> </div>
+                            <p className='text-base'>{preparing_time}</p>
+                        </div>
+                         <div className='flex items-center gap-2'>
+                          <div className='text-xl'> <AiOutlineFire /> </div>
+                            <p className='text-base'>{calories}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <button className='btn rounded-full md:px-4 md:pt-3 md:pb-2 p-2 text-sm md:text-xl font-semibold primary tittle mt-4'>Want to Cook</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
